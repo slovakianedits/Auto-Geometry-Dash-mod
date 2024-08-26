35747845361cc03e0da90550e7decded0e9614a7
slovakianedits <editzslovakian@gmail.com>
2024-08-26 16:16:37 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+//That's not made for cheats lol...
+const fileLoad = "geometrydash.exe";
+let load = "exe_file";
+//Program
+const spike = "^";
+const cube = "^";
+//Looks
+const see ="spike = 'shape'";
+const see2 = "cube = 'square'";
+//
+var player
+if(spike > cube) console.log('jump');
+//Jump <spike>
+//Jump <cube>
+var spikeLook
+if(see > see2) console.log('understanding what is this');
+if ("Understanding what is this") console.log('Jump / Do nothing');
